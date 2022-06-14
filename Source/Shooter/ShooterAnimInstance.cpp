d9a95084b658786de317398dcb8356443982904d
moonabyss <d-v@ukr.net>
2022-06-14 15:15:05 +0300

    03.01. The Anim Instance

---
+++ Source/Shooter/ShooterAnimInstance.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAnimInstance.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PrintStrings.h"
+#include "ShooterCharacter.h"
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+}
+
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (ShooterCharacter == nullptr)
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
+
+	if (ShooterCharacter == nullptr) return;
+	
+	// Get the lateral speed of the character from velocity
+	FVector Velocity{ ShooterCharacter->GetVelocity() };
+	Velocity.Z = 0;
+	Speed = Velocity.Size();
+
+	// Is the character in the air?
+	bInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+
+	// Is the character accelerating?
+	bAccelerating = ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+	
+	printf(0, "Speed = %f", Speed);
+	printf(1, "bInAir = %d", bInAir);
+	printf(2, "bAccelerating = %d", bAccelerating);
+}
---
+++ Source/Shooter/ShooterAnimInstance.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+class AShooterCharacter;
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+
+	/** Update parameters every frame */
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+private:
+	UPROPERTY(Category = "Movement", BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	AShooterCharacter* ShooterCharacter;
+
+	/** The speed of the Character */
+	UPROPERTY(Category = "Movement", BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	/** Whether or not the character is in the air */
+	UPROPERTY(Category = "Movement", BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	bool bInAir;
+
+	/** Whether or not the character is moving */
+	UPROPERTY(Category = "Movement", BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	bool bAccelerating;
+};
